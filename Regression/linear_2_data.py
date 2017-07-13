3472c12b08ad90900540ac9b178868d359ee263c
shagu951 <norshankar@gmail.com>
2017-07-13 20:28:15 +0200

    Update linear_2_data.py
    clean up

--- Regression/linear_2_data.py
+++ Regression/linear_2_data.py
@@ -1,5 +1,4 @@
 import scipy as sp
-
 import matplotlib.pyplot as plt
 
 data = sp.genfromtxt("web_traffic.tsv", delimiter="\t")
@@ -13,7 +12,6 @@ plt.scatter(x,y)
 plt.title("Web traffic over the last month")
 plt.xlabel("Time")
 plt.ylabel("Hits/hour")
-
 inflection = int(3.5*7*24) # calculate the inflection point in hours
 xa = x[:inflection] # data before the inflection point
 ya = y[:inflection]
@@ -21,13 +19,9 @@ xb = x[inflection:] # data after
 yb = y[inflection:]
 fa = sp.poly1d(sp.polyfit(xa, ya, 1))
 fb = sp.poly1d(sp.polyfit(xb, yb, 1))
-
-
 plt.xticks([w*7*24 for w in range(10)],#list 
  ['week %i'%w for w in range(10)])#names in the axis
-
 #polyfit returns several co-eff if full=True otherwise it will return only fp1
-
 #once we get the optimal co-eff , evaluate  those co-eff in the poly1d 
 f1 = sp.poly1d(fa)
 f2 = sp.poly1d(fb)
